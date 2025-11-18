2dc6f260d0fdfba4df4afd682256523535b0e346
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-18 16:24:12 +0300

    feature : add a unit test to the project
    * Added a unit test to the project

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void test() {
+        int a = 1;
+        assertEquals(1, a);
+    }
+}
